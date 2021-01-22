8a2e6e23052aa2c8910961d3b6b4d7f0975c202d
DariWolferin <65591301+DariWolferin@users.noreply.github.com>
2021-01-22 13:28:14 +0300

    Update scriptMoto.js

--- js/scriptMoto.js
+++ js/scriptMoto.js
@@ -29,3 +29,16 @@ ibg();
         })
       }
     })();
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for(let anchor of anchors) {
+  anchor.addEventListener('click', function(event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
